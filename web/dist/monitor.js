c567bade5b09fbd121103c3bd605325bb8c35349
Nathan Kleekamp <nkleekamp@gmail.com>
2020-04-07 14:37:18 -0400

    add check for error page and remove iife

--- web/dist/monitor.js
+++ web/dist/monitor.js
@@ -1,68 +1,68 @@
 "use strict";
-(function () {
-    function textNodesUnder(elem) {
-        var a = [];
-        var accept = {
-            acceptNode: function (node) {
-                if (node.textContent && node.textContent.trim()) {
-                    return NodeFilter.FILTER_ACCEPT;
-                }
-                return NodeFilter.FILTER_REJECT;
+function textNodesUnder(elem) {
+    var a = [];
+    var accept = {
+        acceptNode: function (node) {
+            if (node.textContent && node.textContent.trim()) {
+                return NodeFilter.FILTER_ACCEPT;
             }
-        };
-        var walker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT, accept, false);
-        var n = walker.nextNode();
-        if (!n) {
-            return a;
+            return NodeFilter.FILTER_REJECT;
         }
-        do {
-            a.push(n);
-        } while (n = walker.nextNode());
+    };
+    var walker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT, accept, false);
+    var n = walker.nextNode();
+    if (!n) {
         return a;
     }
-    function isHidden(elem) {
-        var style = window.getComputedStyle(elem);
-        return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
-            || elem.hidden
-            || style.visibility === "hidden"
-            || style.opacity === "0";
-    }
-    function containsSubstring(arr, substring) {
-        return arr.length > 0 && arr.find(function (str) { return str.toLowerCase().includes(substring); });
-    }
-    function getRandomMs(minMs, maxMs) {
-        if (minMs === void 0) { minMs = 2000; }
-        if (maxMs === void 0) { maxMs = 5000; }
-        return Math.random() * (maxMs - minMs) + minMs;
-    }
-    function getTextNodes(selector) {
-        var deliveryTimeForm = document.querySelector(selector);
-        var result = [];
-        if (!deliveryTimeForm) {
+    do {
+        a.push(n);
+    } while (n = walker.nextNode());
+    return a;
+}
+function isHidden(elem) {
+    var style = window.getComputedStyle(elem);
+    return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
+        || elem.hidden
+        || style.visibility === "hidden"
+        || style.opacity === "0";
+}
+function containsSubstring(arr, substring) {
+    return arr.length > 0 && arr.find(function (str) { return str.toLowerCase().includes(substring); });
+}
+function getRandomMs(minMs, maxMs) {
+    if (minMs === void 0) { minMs = 2000; }
+    if (maxMs === void 0) { maxMs = 5000; }
+    return Math.random() * (maxMs - minMs) + minMs;
+}
+function getTextNodes(selector) {
+    var deliveryTimeForm = document.querySelector(selector);
+    var result = [];
+    if (!deliveryTimeForm) {
+        if (document.body.id !== "desktop-internal-error") {
             console.log("No delivery form. Reloading...");
             location.reload();
-            return result;
         }
-        var nodes = textNodesUnder(deliveryTimeForm);
-        nodes.forEach(function (n) {
-            var parent = n.parentElement;
-            if (!parent || isHidden(parent))
-                return;
-            if ((n === null || n === void 0 ? void 0 : n.textContent) && typeof n.textContent === "string") {
-                result.push(n.textContent.trim());
-            }
-        });
         return result;
     }
-    var nonEmptyText = getTextNodes("#delivery-slot-form");
-    var NO_DELIVERY = "no delivery windows available";
-    if (nonEmptyText.length && containsSubstring(nonEmptyText, NO_DELIVERY)) {
-        var ms = getRandomMs();
-        console.log("Reloading in " + ms + " ms...");
-        setTimeout(function () { return location.reload(); }, ms);
-    }
-    else {
-        console.log("order");
-    }
-}());
+    var nodes = textNodesUnder(deliveryTimeForm);
+    nodes.forEach(function (n) {
+        var parent = n.parentElement;
+        if (!parent || isHidden(parent))
+            return;
+        if ((n === null || n === void 0 ? void 0 : n.textContent) && typeof n.textContent === "string") {
+            result.push(n.textContent.trim());
+        }
+    });
+    return result;
+}
+var nonEmptyText = getTextNodes("#delivery-slot-form");
+var NO_DELIVERY = "no delivery windows available";
+if (nonEmptyText.length && containsSubstring(nonEmptyText, NO_DELIVERY)) {
+    var ms = getRandomMs();
+    console.log("Reloading in " + ms + " ms...");
+    setTimeout(function () { return location.reload(); }, ms);
+}
+else {
+    console.log("order");
+}
 //# sourceMappingURL=monitor.js.map
--- web/src/monitor.ts
+++ web/src/monitor.ts
@@ -1,71 +1,71 @@
-(function() {
-  function textNodesUnder(elem: HTMLElement) {
-    const a: Node[] = [];
-    const accept: NodeFilter = {
-      acceptNode: function(node: Node) {
-        if (node.textContent && node.textContent.trim()) {
-          return NodeFilter.FILTER_ACCEPT;
-        }
-        return NodeFilter.FILTER_REJECT;
+function textNodesUnder(elem: HTMLElement) {
+  const a: Node[] = [];
+  const accept: NodeFilter = {
+    acceptNode: function(node: Node) {
+      if (node.textContent && node.textContent.trim()) {
+        return NodeFilter.FILTER_ACCEPT;
       }
-    };
-    const walker: TreeWalker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT, accept, false);
-    let n = walker.nextNode();
-    if (!n) {
-      return a;
+      return NodeFilter.FILTER_REJECT;
     }
-    do {
-      a.push(n)
-    } while (n = walker.nextNode());
+  };
+  const walker: TreeWalker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT, accept, false);
+  let n = walker.nextNode();
+  if (!n) {
     return a;
   }
+  do {
+    a.push(n)
+  } while (n = walker.nextNode());
+  return a;
+}
 
-  function isHidden(elem: HTMLElement) {
-    const style = window.getComputedStyle(elem);
-    return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
-      || elem.hidden
-      || style.visibility === "hidden"
-      || style.opacity === "0";
-  }
+function isHidden(elem: HTMLElement) {
+  const style = window.getComputedStyle(elem);
+  return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
+    || elem.hidden
+    || style.visibility === "hidden"
+    || style.opacity === "0";
+}
 
-  function containsSubstring(arr: string[], substring: string) {
-    return arr.length > 0 && arr.find((str: string) => str.toLowerCase().includes(substring));
-  }
+function containsSubstring(arr: string[], substring: string) {
+  return arr.length > 0 && arr.find((str: string) => str.toLowerCase().includes(substring));
+}
 
-  function getRandomMs(minMs = 2000, maxMs = 5000) {
-    return Math.random() * (maxMs - minMs) + minMs;
-  }
+function getRandomMs(minMs = 2000, maxMs = 5000) {
+  return Math.random() * (maxMs - minMs) + minMs;
+}
 
-  function getTextNodes(selector: string): string[] {
-    const deliveryTimeForm: HTMLElement | null = document.querySelector(selector);
-    let result: string[] = [];
+function getTextNodes(selector: string): string[] {
+  const deliveryTimeForm: HTMLElement | null = document.querySelector(selector);
+  let result: string[] = [];
 
-    if (!deliveryTimeForm) {
+  if (!deliveryTimeForm) {
+    if (document.body.id !== "desktop-internal-error") {
       console.log("No delivery form. Reloading...");
       location.reload();
-      return result;
     }
-
-    const nodes: Node[] = textNodesUnder(deliveryTimeForm);
-    nodes.forEach((n: Node) => {
-      const parent: HTMLElement | null = n.parentElement;
-      if (!parent || isHidden(parent)) return;
-      if (n?.textContent && typeof n.textContent === "string") {
-        result.push(n.textContent.trim());
-      }
-    });
-
     return result;
   }
 
-  const nonEmptyText = getTextNodes("#delivery-slot-form");
-  const NO_DELIVERY = "no delivery windows available";
+  const nodes: Node[] = textNodesUnder(deliveryTimeForm);
+  nodes.forEach((n: Node) => {
+    const parent: HTMLElement | null = n.parentElement;
+    if (!parent || isHidden(parent)) return;
+    if (n?.textContent && typeof n.textContent === "string") {
+      result.push(n.textContent.trim());
+    }
+  });
 
-  if (nonEmptyText.length && containsSubstring(nonEmptyText, NO_DELIVERY)) {
-    const ms = getRandomMs();
-    console.log(`Reloading in ${ms} ms...`);
-    setTimeout(() => location.reload(), ms);
-  } else {
-    console.log("order");
-  }
-}());
+  return result;
+}
+
+const nonEmptyText = getTextNodes("#delivery-slot-form");
+const NO_DELIVERY = "no delivery windows available";
+
+if (nonEmptyText.length && containsSubstring(nonEmptyText, NO_DELIVERY)) {
+  const ms = getRandomMs();
+  console.log(`Reloading in ${ms} ms...`);
+  setTimeout(() => location.reload(), ms);
+} else {
+  console.log("order");
+}
